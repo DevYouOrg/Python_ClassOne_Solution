466afa369f9dfe36351f1497fdcb66a6e2f16cd5
Hemory Phifer <hphife@yahoo.com>
2021-11-05 22:25:55 -0400

    Solution

---
+++ main.py
@@ -0,0 +1,20 @@
+# Exercise One
+
+name = "Charlie"
+current_year = 2022
+gas_price = 3.25
+fave_quote = "If you are not happy with the life you are living. Change it!"
+
+
+# Exercise Two
+
+print("Keep up the good work")
+print(name)
+print(gas_price)
+
+# Exercise Three
+
+first_name = "Billie"
+last_name = "Holiday"
+
+print(f"Hello {first_name} {last_name} it is nice to meet you!")
